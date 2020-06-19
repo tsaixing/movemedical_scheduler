1019417f5c9e368142323b96cc8bf6ec8404b91e
Tsai-Xing Wong <tsaixing@gmail.com>
2020-06-18 23:08:30 -0700

    Updates

---
+++ precache-manifest.a575efc3b18b5bf596a39ab6e25c7713.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f31bd2d1898a23e279ce77764779db86",
+    "url": "/movemedical_scheduler/index.html"
+  },
+  {
+    "revision": "d3c57e205e560b7d10ee",
+    "url": "/movemedical_scheduler/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4b41a2408f645c61ef20",
+    "url": "/movemedical_scheduler/static/js/2.1dee597b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movemedical_scheduler/static/js/2.1dee597b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3c57e205e560b7d10ee",
+    "url": "/movemedical_scheduler/static/js/main.125d2f78.chunk.js"
+  },
+  {
+    "revision": "df0482b6e31a81e14ade",
+    "url": "/movemedical_scheduler/static/js/runtime-main.69c17824.js"
+  }
+]);
